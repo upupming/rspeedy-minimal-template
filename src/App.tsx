841af9597100146fc18f62fb0c4c7ce9e1eebcbc
Yiming Li <yimingli.cs@gmail.com>
2026-02-26 20:26:32 +0800

    feat: upgrade rspeedy

--- src/App.tsx
+++ src/App.tsx
@@ -19,22 +19,25 @@ export function App() {
 
   return (
     <view>
-      <view className='Background' />
-      <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
-            {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
+      <view className="Background" />
+      <view className="App">
+        <view className="Banner">
+          <view className="Logo" bindtap={onTap}>
+            {alterLogo ? (
+              <image src={reactLynxLogo} className="Logo--react" />
+            ) : (
+              <image src={lynxLogo} className="Logo--lynx" />
+            )}
           </view>
-          <text className='Title'>React</text>
-          <text className='Subtitle'>on Lynx</text>
+          <text className="Title">React</text>
+          <text className="Subtitle">on Lynx</text>
         </view>
-        <view className='Content'>
-          <image src={arrow} className='Arrow' />
-          <text className='Description'>Tap the logo and have fun!</text>
-          <text className='Hint'>
-            Edit<text
+        <view className="Content">
+          <image src={arrow} className="Arrow" />
+          <text className="Description">Tap the logo and have fun!</text>
+          <text className="Hint">
+            Edit
+            <text
               style={{
                 fontStyle: 'italic',
                 color: 'rgba(255, 255, 255, 0.85)',
